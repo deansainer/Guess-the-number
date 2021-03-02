d69d036ebec1aab6f6253751e5bf0217b24bcb00
deansainer <76252799+determind2k@users.noreply.github.com>
2021-03-02 16:49:55 +0100

    Add files via upload

---
+++ Guess the number.py
@@ -0,0 +1,21 @@
+import random
+
+while True:
+    count = 0
+    randnum = random.randint(1, 5)
+    mynum = int(input('-----------------------------\nPlease type your number (1-5): '))
+    count += 1
+    amiwinner = False
+    if randnum == mynum:
+        amiwinner = True
+        print(f'Congratulations, you guessed our number!')
+        print(f'Your number is: {mynum}\nOur number is: {randnum}')
+        break
+    else:
+        print('Sorry, you didnt guess our number')
+    print(f'Your number is: {mynum}\nOur number is: {randnum}')
+    if amiwinner == False:
+        if (mynum < randnum):
+            print(f'Your number was {randnum - mynum} units lower than ours, try again')
+        else:
+            print(f'Your number was {mynum - randnum} units higher than ours, try again ')
